b916d26ca987a994114d3a3d443f5362c6a62993
sidoschn <dominik.schneidereit@fau.de>
2026-04-20 17:00:56 +0200

    added some testContentfiles

--- testFile.cpp
+++ testFile.cpp
@@ -1,13 +1,38 @@
 
 #include <iostream>
 #include "httplib.h"
+#include <fstream>
 
 httplib::Server svr;
 
 int main(){
-    std::cout << "test2";
+    std::cout << "test5";
+    
+    
+    svr.Get("/viewer", [](const httplib::Request &, httplib::Response &res) {
+        std::string dataFileName = "testTable.csv";
+        std::ifstream contentFile;
+    
+        contentFile.open(dataFileName);
+        if(!contentFile){
+            std::cout << "Datei nicht geöffnet:" << dataFileName;
+        }
 
-    svr.Get("/viewer", [](const httplib::Request &, httplib::Response &res) {res.set_content("LocoViewerLight is being built here", "text/plain");});
+        
+        
+        std::string fileContent;
+        char fileChar;
+        
+
+        while (contentFile.get(fileChar)){
+            fileContent += fileChar;
+        }
+
+        if (contentFile.is_open()){
+            contentFile.close();
+        }    
+        res.set_content(fileContent, "text/plain");
+    });
     svr.listen("0.0.0.0", 8234);
     std::cout << "test3";
     return 0;
